1e8ce30455e6a81c9268b25a3a947369df90b8e3
paovang <2018programmer@gmail.com>
2020-06-25 15:09:48 +0700

    update

--- app/Http/Controllers/ApiAdminController.php
+++ app/Http/Controllers/ApiAdminController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class ApiAdminController extends Controller
 {
     //
     public function ListRestaurants(){
-        return 'Good';
+        $user = User::all();
+        return response()->json([
+            'data' => $user
+        ]);
     }
 }
