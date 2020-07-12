33db40f9192362b05b34456ce36ac5fbe36ac0bd
Bee <beetimberlake2@gmail.com>
2020-07-12 17:42:05 +0700

    Checking proudct amount v1.0

--- app/models/product.php
+++ app/models/product.php
@@ -6,17 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class product extends Model
 {
-    public static function AddProducts($item, $restaurantId){
+    protected $casts = [
+        'price' => 'double'
+    ];
+
+    public static function AddProducts($item, $restaurantId)
+    {
         $fileName = substr($item->file, 0, strrpos($item->file, "."));
-        $oldPath = 'images/uploadFile/'. $item->file; 
+        $oldPath = 'images/uploadFile/' . $item->file;
 
         $fileExtension = \File::extension($oldPath);
-        $newName = $fileName.'.'.$fileExtension;
-        $newPathWithName = 'images/restaurants/file_menu/'.$newName;
+        $newName = $fileName . '.' . $fileExtension;
+        $newPathWithName = 'images/restaurants/file_menu/' . $newName;
 
-        if(\File::copy($oldPath , $newPathWithName)){
-            if(isset($item->file)){
-                unlink(public_path().'/images/uploadFile/' . $item->file);
+        if (\File::copy($oldPath, $newPathWithName)) {
+            if (isset($item->file)) {
+                unlink(public_path() . '/images/uploadFile/' . $item->file);
             }
         }
 
@@ -29,47 +34,49 @@ class product extends Model
         $add->file = $item->file;
         $add->restaurant_id = $restaurantId;
         $add->save();
-        if($add){
+        if ($add) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
-    public static function EditProducts($item, $id){
+
+    public static function EditProducts($item, $id)
+    {
         $edit_product = self::find($id);
 
-        if(isset($item->file)){
+        if (isset($item->file)) {
             $fileName = substr($item->file, 0, strrpos($item->file, "."));
-            $oldPath = 'images/uploadFile/'. $item->file; 
+            $oldPath = 'images/uploadFile/' . $item->file;
 
             $fileExtension = \File::extension($oldPath);
-            $newName = $fileName.'.'.$fileExtension;
-            $newPathWithName = 'images/restaurants/file_menu/'.$newName;
+            $newName = $fileName . '.' . $fileExtension;
+            $newPathWithName = 'images/restaurants/file_menu/' . $newName;
 
-            if(\File::copy($oldPath , $newPathWithName)){
-                if(isset($item->file)){
-                    unlink(public_path().'/images/uploadFile/' . $item->file);
+            if (\File::copy($oldPath, $newPathWithName)) {
+                if (isset($item->file)) {
+                    unlink(public_path() . '/images/uploadFile/' . $item->file);
                 }
             }
-            if(isset($edit_product->file)){
-                unlink(public_path().'/images/restaurants/file_menu/' . $edit_product->file);
+            if (isset($edit_product->file)) {
+                unlink(public_path() . '/images/restaurants/file_menu/' . $edit_product->file);
             }
             $edit_product->file = $item->file;
         }
 
-        
+
         $edit_product->product_name = $item->product_name;
         $edit_product->amount = $item->amount;
         $edit_product->price = $item->price;
         $edit_product->product_type_id = $item->proTypeId;
         $edit_product->unit_id = $item->unitId;
         $edit_product->save();
-        if($edit_product){
+        if ($edit_product) {
             return true;
-        }else{
+        } else {
             return false;
         }
 
     }
-    
+
 }
